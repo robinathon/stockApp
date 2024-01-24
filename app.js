efc08e5e44dd2e17806dc9b4dced94c54d68d0de
robinathon <allenrobinsankalp@gmail.com>
2024-01-24 12:52:31 +0530

    some changes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cookieParser());
 
 async function runDownloadScript() {
   try {
+    //this will fectch latest last 50 days data from BhavCopy BSE
     await downloadAndProcessData(); //do not incldue this line while deploying on any free deploying platform, it will exceed the free-tier memory limit.
     console.log("Data download and processing completed.");
   } catch (error) {
